01bd1a57c6716633deb37e320b82698c395ab532
jtman <jtman876@gmail.com>
2026-04-26 19:07:30 -0500

    Fix graph and queue, add total distance to each node, and complete dijkstra's pathing

--- dijkstra.c
+++ dijkstra.c
@@ -6,10 +6,9 @@
 #define INF 1000
 
 typedef struct node {
-    int vertex;
-    int weight;
-    int distance;
-
+    int          vertex;
+    int          weight;
+    int          distance;
     struct node *next;
 } Node;
 
@@ -21,11 +20,17 @@ typedef struct {
 } Graph;
 
 typedef struct {
-    int size;
-    int max;
-
+    int   size;
+    int   max;
     Node *min_heap;
 } PriorityQueue;
+
+typedef struct tree_node {
+    int                vertex;
+    int                path;
+    struct tree_node  *parent;
+    struct tree_node **children;
+} TreeNode;
 
 Graph input();
 void  swap_nodes(Node *a, Node *b);
@@ -87,7 +92,7 @@ Graph input() {
     for (int i = 0; i < num_edges; i++) {
         int u = next_alnum();
         int v = next_alnum();
-        int w = next_alnum();
+        int w = next_number();
         add_edge(&graph, u, v, w);
     }
     printf("Enter the source node: ");
@@ -132,9 +137,9 @@ Node *create_node(int v, int w) {
 }
 
 void add_edge(Graph *graph, int u, int v, int w) {
-    Node *new_node = create_node(u, w);
-    new_node->next = graph->adj_lists[v];
-    graph->adj_lists[v] = new_node;
+    Node *new_node = create_node(v, w);
+    new_node->next = graph->adj_lists[u];
+    graph->adj_lists[u] = new_node;
 }
 
 /** Helper functions **/
@@ -164,63 +169,166 @@ void insert_queue(PriorityQueue *queue, Node node) {
 }
 
 void heapify(PriorityQueue *queue, int i) {
-    int largest = i;
+    int smallest = i;
     int l = 2 * i + 1;
     int r = 2 * i + 2;
 
     if (l < queue->size &&
-        queue->min_heap[l].distance > queue->min_heap[largest].distance) {
-        largest = l;
+        queue->min_heap[l].distance < queue->min_heap[smallest].distance) {
+        smallest = l;
     }
     if (r < queue->size &&
-        queue->min_heap[r].distance > queue->min_heap[largest].distance) {
-        largest = r;
+        queue->min_heap[r].distance < queue->min_heap[smallest].distance) {
+        smallest = r;
     }
-    if (largest != i) {
-        swap_nodes(&queue->min_heap[i], &queue->min_heap[largest]);
-        heapify(queue, largest);
+    if (smallest != i) {
+        swap_nodes(&queue->min_heap[i], &queue->min_heap[smallest]);
+        heapify(queue, smallest);
     }
 }
 
+Node dequeue(PriorityQueue *queue) {
+    Node min = queue->min_heap[0];
+    queue->min_heap[0] = queue->min_heap[queue->size - 1];
+    queue->size -= 1;
+    heapify(queue, 0);
+    return min;
+}
+
 void update_queue(PriorityQueue *queue, Node *node) {
+    int current;
     for (int i = 0; i < queue->size; i++) {
         if (queue->min_heap[i].vertex == node->vertex) {
             queue->min_heap[i].distance = node->distance;
+            // heapify(queue, i);
+            // return;
+            current = i;
+            break;
         }
     }
+    while (current != 0) {
+        int parent = (current - 1) / 2;
+        if (queue->min_heap[current].distance <
+            queue->min_heap[parent].distance) {
+            swap_nodes(&queue->min_heap[current], &queue->min_heap[parent]);
+            current = parent;
+        } else {
+            break;
+        }
+    }
+}
+
+void print_path(int parent[], int v) {
+    if (v == -1) {
+        return;
+    }
+    print_path(parent, parent[v]);
+    printf("%c ", v);
 }
 
 /** Dijkstra's Algorithm **/
 void dijkstra(Graph graph) {
+    printf("Starting dijkstra's\n");
+    print_graph(graph);
     PriorityQueue queue;
+    queue.size = 0;
+    queue.max = graph.vertices_count;
+    queue.min_heap = malloc(sizeof(Node) * graph.vertices_count);
+    int parent[graph.vertices_count];
+    int visited[graph.vertices_count];
     for (int i = 0; i < graph.vertices_count; i++) {
-        Node *parent = NULL;
-        int   path = (i == graph.source) ? 0 : INF;
+        int path = (i == graph.source) ? 0 : INF;
+        parent[i] = -1;
+        visited[i] = 0;
 
+        // TODO: nodes in queue will be destoryed at end of function
         Node dest = {i, 0, path, NULL};
+        printf("Build vars.\n");
         insert_queue(&queue, dest);
+        printf("Inserted queue.\n");
     }
+    printf("Priority queue initialized.\n");
+
+    // for (int i = 0; i < graph.vertices_count; i++) {
+    //     if (queue.min_heap[i].distance == 0) {
+    //         printf("Source: %d", queue.min_heap[i].vertex);
+    //     }
+    // }
 
     // init tree
+
+    Node     *head;
+    TreeNode *treeHead;
     for (int i = 0; i < graph.vertices_count; i++) {
+        // printf("Popping min node.\n");
         Node node = dequeue(&queue);
+        visited[node.vertex] = 1;
         // insert the next node into the tree from its source
         // loop over neighbors of u
         /*
          * All neighbors are initialized to have a distance of INF
-         * A node dequeued has no weight
+         * All nodes are initialized with no weight and no next node
          * Not all vertices will be initialized with a distance of INF?
+         * Once a node is dequeued, it should have a parent. If it doesn't, it
+         * is the head
+         *
          */
+
+        // Node children[VERTICES];
+        // if (!node.next) { // TODO: what is the lifetime of node? pointer?
+        //     head = &node;
+        // } else {
+        //     children[i] = node;
+        // }
+        //
+        // TreeNode *treeNode = (TreeNode *)malloc(sizeof(TreeNode *));
+        // treeNode->vertex = node.vertex;
+        // treeNode->parent = node.next;
+        // treeNode->parent->children[i] = &treeNode;
+        //
+        // if (!node.next) {
+        // }
+        //
+        // TODO: INPUT GRAPH
+        // a b 5 a c 2 b c 1 b d 4 b e 2 c e 7 d e 6 d f 3 e f 1
+        // 1 2 5 1 3 2 2 3 1 2 4 4 2 5 2 3 5 7 4 5 6 4 6 3 5 6 1
+
+        // printf("Checking min node %d with distance %d\n", node.vertex,
+        //        node.distance);
         Node *neighbor = graph.adj_lists[node.vertex];
+        // Update distance for every neighbor
         while (neighbor) {
             // if current node distance + the edge is better than neighbor dist
-            if (node.distance + neighbor->weight < neighbor->distance) {
+            printf("%c has neighbor %c\n", node.vertex, neighbor->vertex);
+            printf("%d < %d\n", node.distance + neighbor->weight,
+                   neighbor->distance);
+            if (!visited[neighbor->vertex] &&
+                node.distance + neighbor->weight < neighbor->distance) {
                 neighbor->distance = node.distance + neighbor->weight;
-                Node *parent = &node;
+                printf("New distance for %d: %d\n", neighbor->vertex,
+                       neighbor->distance);
+                parent[neighbor->vertex] = node.vertex;
+                // neighbor->next = &node; // Neighbor's parent is node
+                // Problem: we are altering the graph. We need to change the
+                // queue Or, we can have a predecessor array
                 update_queue(&queue, neighbor);
+                printf("Now the first node is %d\n", queue.min_heap[0].vertex);
             }
             neighbor = neighbor->next;
         }
     }
+    printf("dijkstra complete\n");
+    for (int i = '2'; i < '7'; i++) {
+        print_path(parent, i);
+        printf("\n");
+    }
     // return tree
+}
+
+void traversal() {
+    // All weights start at 0
+    // Have a list of children to recursively traverse up the tree
+    // Print each node backward as recursion finishes.
+    // Set the weight to 1 if a node has been visited.
+    // You can't start a recursion on a node that equals 1
 }
